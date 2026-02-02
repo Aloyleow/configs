e3f35588d68e754825d9cdb50a8bf893c58148cd
aloyleow <aloyleow91@gmail.com>
2026-02-02 21:45:31 +0800

    fix: conform and glovbal tab options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,10 @@
 vim.opt.number = true
 
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
